bb9d578f0c426f9d3f4807d80362fda672145a38
dynamoro <dynamoro+database@hotmail.com>
2023-12-27 10:55:14 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//Settings
+window.onload = () => {
+    const btn = document.getElementById("btn-browse-collection");
+    btn.addEventListener("click", () => {
+        document.getElementById("membership-a").scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+
+    const quantities = Array.from(document.getElementsByClassName("quantity"));
+    quantities.forEach((quantity) => {
+        quantity.addEventListener("change",  (e) => {
+            console.log(e.target.value);
+            const product = quantity.closest('.product');
+            if (product) { 
+                product
+                .getElementsByClassName('snipcart-add-item')?.[0]?.setAttribute('data-item-quantity', e.target.value)
+            }
+        });
+    });
+};
